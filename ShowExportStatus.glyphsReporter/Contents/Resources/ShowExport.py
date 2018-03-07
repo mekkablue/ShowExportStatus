a380dc93f1bf2bc3b79c9dfa70c9c2065656e1a3
schriftgestalt <georg.seifert@mac.com>
2018-03-07 20:12:06 +0100

    use new plugin API and simplify
    - draw the cross in the background (there is no API to draw in the
    foreground for inactive layers
    - don’t draw the outline ourself because there might be some special
    drawing (color) going on

--- ShowExportStatus.glyphsReporter/Contents/Resources/ShowExport.py
+++ ShowExportStatus.glyphsReporter/Contents/Resources/ShowExport.py
@@ -1,76 +1,15 @@
-#!/usr/bin/env python
 # encoding: utf-8
 
 import objc
-from Foundation import *
-from AppKit import *
-import sys, os, re, math
+from GlyphsApp import *
+from GlyphsApp.plugins import *
+import sys, os, re, math, traceback
 
-MainBundle = NSBundle.mainBundle()
-path = MainBundle.bundlePath() + "/Contents/Scripts"
-if not path in sys.path:
-	sys.path.append( path )
+class ShowExport(ReporterPlugin):
+	
+	def settings(self):
+		self.menuName = "Export Status"
 
-import GlyphsApp
-
-GlyphsReporterProtocol = objc.protocolNamed( "GlyphsReporter" )
-
-class ShowExport ( NSObject, GlyphsReporterProtocol ):
-	
-	def init( self ):
-		"""
-		Put any initializations you want to make here.
-		"""
-		try:
-			#Bundle = NSBundle.bundleForClass_( NSClassFromString( self.className() ));
-			return self
-		except Exception as e:
-			self.logToConsole( "init: %s" % str(e) )
-	
-	def interfaceVersion( self ):
-		"""
-		Distinguishes the API version the plugin was built for. 
-		Return 1.
-		"""
-		try:
-			return 1
-		except Exception as e:
-			self.logToConsole( "interfaceVersion: %s" % str(e) )
-	
-	def title( self ):
-		"""
-		This is the name as it appears in the menu in combination with 'Show'.
-		E.g. 'return "Nodes"' will make the menu item read "Show Nodes".
-		"""
-		try:
-			return "Export Status"
-		except Exception as e:
-			self.logToConsole( "title: %s" % str(e) )
-	
-	def keyEquivalent( self ):
-		"""
-		The key for the keyboard shortcut. Set modifier keys in modifierMask() further below.
-		Pretty tricky to find a shortcut that is not taken yet, so be careful.
-		If you are not sure, use 'return None'. Users can set their own shortcuts in System Prefs.
-		"""
-		try:
-			return ""
-		except Exception as e:
-			self.logToConsole( "keyEquivalent: %s" % str(e) )
-	
-	def modifierMask( self ):
-		"""
-		Use any combination of these to determine the modifier keys for your default shortcut:
-			return NSShiftKeyMask | NSControlKeyMask | NSCommandKeyMask | NSAlternateKeyMask
-		Or:
-			return 0
-		... if you do not want to set a shortcut.
-		"""
-		try:
-			return 0
-		except Exception as e:
-			self.logToConsole( "modifierMask: %s" % str(e) )
-	
 	def drawCrossOverLayer( self, Layer, lineWidth ):
 		try:
 			# determine italic angle:
@@ -104,20 +43,7 @@ class ShowExport ( NSObject, GlyphsReporterProtocol ):
 		except Exception as e:
 			self.logToConsole( "drawCrossOverLayer_: %s" % str(e) )
 
-	def bezierPathComp( self, thisLayer ):
-		"""Compatibility method for bezierPath before v2.3."""
-		layerBezierPath = NSBezierPath.bezierPath()
-		try:
-			layerBezierPath.appendBezierPath_( thisLayer.bezierPath() ) # until v2.2
-		except Exception as e:
-			layerBezierPath.appendBezierPath_( thisLayer.bezierPath ) # v2.3+
-		
-		for thisComponent in thisLayer.components:
-			layerBezierPath.appendBezierPath_( thisComponent.bezierPath() )
-		
-		return layerBezierPath
-
-	def drawForegroundForLayer_( self, Layer ):
+	def background( self, Layer ):
 		"""
 		Whatever you draw here will be displayed IN FRONT OF the paths.
 		Setting a color:
@@ -143,75 +69,18 @@ class ShowExport ( NSObject, GlyphsReporterProtocol ):
 				self.drawCrossOverLayer( Layer, 1.0 / self.getScale() )
 		except Exception as e:
 			self.logToConsole( "drawForegroundForLayer_: %s" % str(e) )
-	
-	def drawBackgroundForLayer_( self, Layer ):
-		"""
-		Whatever you draw here will be displayed BEHIND the paths.
-		"""
-		try:
-			pass
-		except Exception as e:
-			self.logToConsole( "drawBackgroundForLayer_: %s" % str(e) )
-	
-	def drawBackgroundForInactiveLayer_( self, Layer ):
+		
+	def inactiveLayers(self, Layer):
 		"""
 		Whatever you draw here will be displayed behind the paths, but for inactive masters.
 		"""
 		try:
 			thisGlyph = Layer.glyph()
-			if thisGlyph:
-				# Color for INACTIVE GLYPH IN EDIT VIEW
-				if self.controller:
-					if not thisGlyph.export:
-						# set the drawing color to black:
-						NSColor.darkGrayColor().set()
-						self.bezierPathComp(Layer).fill()
-						self.drawCrossOverLayer( Layer, 1.0 / self.getScale() )
-					#else:
-					#	pass
-
-				# Color for GLYPH IN PREVIEW:	
-				else:
-					if not thisGlyph.export:
-						NSColor.darkGrayColor().set()
-						# check for background color (can be black or white):
-						if NSUserDefaults.standardUserDefaults().boolForKey_("GSPreview_Black"):
-							# set the drawing color to white if preview background is black:
-							NSColor.lightGrayColor().set()
-						self.bezierPathComp(Layer).fill()
-						self.drawCrossOverLayer( Layer, 10.0 )
-					#else:
-					#	NSColor.blackColor().set()
-					#	# check for background color (can be black or white):
-					#	if NSUserDefaults.standardUserDefaults().boolForKey_("GSPreview_Black"):
-					#		# set the drawing color to white if preview background is black:
-					#		NSColor.whiteColor().set()
-					#	print 0
-					#	bezierPaths = self.bezierPathComp(Layer)
-					#	if bezierPaths:
-					#		bezierPaths.fill()
-					#	else:
-					#		print thisGlyph.name, Layer.paths
-						
-					
+			print "thisGlyph", thisGlyph
+			if thisGlyph and not thisGlyph.export:
+				self.drawCrossOverLayer(Layer, 1.0 / self.getScale())
 		except Exception as e:
-			self.logToConsole( "drawBackgroundForInactiveLayer_: %s" % str(e) )
-	
-	def drawTextAtPoint( self, text, textPosition, fontSize=9.0, fontColor=NSColor.brownColor() ):
-		"""
-		Use self.drawTextAtPoint( "blabla", myNSPoint ) to display left-aligned text at myNSPoint.
-		"""
-		try:
-			glyphEditView = self.controller.graphicView()
-			currentZoom = self.getScale()
-			fontAttributes = { 
-				NSFontAttributeName: NSFont.labelFontOfSize_( fontSize/currentZoom ),
-				NSForegroundColorAttributeName: fontColor }
-			displayText = NSAttributedString.alloc().initWithString_attributes_( text, fontAttributes )
-			textAlignment = 3 # top left: 6, top center: 7, top right: 8, center left: 3, center center: 4, center right: 5, bottom left: 0, bottom center: 1, bottom right: 2
-			glyphEditView.drawText_atPoint_alignment_( displayText, textPosition, textAlignment )
-		except Exception as e:
-			self.logToConsole( "drawTextAtPoint: %s" % str(e) )
+			print traceback.format_exc()
 	
 	def needsExtraMainOutlineDrawingForInactiveLayer_( self, Layer ):
 		"""
@@ -222,54 +91,5 @@ class ShowExport ( NSObject, GlyphsReporterProtocol ):
 		drawing), which is probably what you want if you are drawing the glyph
 		yourself in self.drawBackgroundForInactiveLayer_().
 		"""
-		if not Layer.glyph().export:
-			return False
-		else:
-			return True
-	
-	def getHandleSize( self ):
-		"""
-		Returns the current handle size as set in user preferences.
-		Use: self.getHandleSize() / self.getScale()
-		to determine the right size for drawing on the canvas.
-		"""
-		try:
-			Selected = NSUserDefaults.standardUserDefaults().integerForKey_( "GSHandleSize" )
-			if Selected == 0:
-				return 5.0
-			elif Selected == 2:
-				return 10.0
-			else:
-				return 7.0 # Regular
-		except Exception as e:
-			self.logToConsole( "getHandleSize: HandleSize defaulting to 7.0. %s" % str(e) )
-			return 7.0
+		return True
 
-	def getScale( self ):
-		"""
-		self.getScale() returns the current scale factor of the Edit View UI.
-		Divide any scalable size by this value in order to keep the same apparent pixel size.
-		"""
-		try:
-			return self.controller.graphicView().scale()
-		except:
-			self.logToConsole( "Scale defaulting to 1.0" )
-			return 1.0
-	
-	def setController_( self, Controller ):
-		"""
-		Use self.controller as object for the current view controller.
-		"""
-		try:
-			self.controller = Controller
-		except Exception as e:
-			self.logToConsole( "Could not set controller" )
-	
-	def logToConsole( self, message ):
-		"""
-		The variable 'message' will be passed to Console.app.
-		Use self.logToConsole( "bla bla" ) for debugging.
-		"""
-		myLog = "Show %s plugin:\n%s" % ( self.title(), message )
-		print myLog
-		NSLog( myLog )
