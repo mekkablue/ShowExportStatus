cf23f2768cb50da6aeca97c32762c27c7d283228
mekkablue <mekka@mekkablue.com>
2024-03-14 10:46:03 +0100

    v2.1 export status in Font View in Glyphs 3.2+

--- ShowExportStatus.glyphsReporter/Contents/Resources/plugin.py
+++ ShowExportStatus.glyphsReporter/Contents/Resources/plugin.py
@@ -16,6 +16,7 @@ import objc
 from math import tan, radians
 from GlyphsApp import *
 from GlyphsApp.plugins import *
+from AppKit import NSColor, NSBezierPath, NSPoint
 
 class ShowExportStatus(ReporterPlugin):
 
@@ -27,18 +28,39 @@ class ShowExportStatus(ReporterPlugin):
 			'es': 'estado de la exportación',
 			'fr': 'état de l’export',
 		})
+
+	@objc.python_method
+	def drawCross(self, xMin, xMax, yMin, yMax, lineWidth=1, italicAngle=0, halfXHeight=0):
+		tangens = tan(italicAngle)
+		italicBottomOffset = tangens * (yMin - halfXHeight)
+		italicTopOffset = tangens * (yMax - halfXHeight)
+		NSColor.redColor().set()
+		cross = NSBezierPath.bezierPath()
+		cross.moveToPoint_(NSPoint(xMin + italicBottomOffset, yMin))
+		cross.lineToPoint_(NSPoint(xMax + italicTopOffset,    yMax))
+		cross.moveToPoint_(NSPoint(xMax + italicBottomOffset, yMin))
+		cross.lineToPoint_(NSPoint(xMin + italicTopOffset,    yMax))
+		cross.setLineWidth_(lineWidth)
+		cross.stroke()
+	
+	def drawFontViewForegroundForLayer_inFrame_(self, layer, frame):
+		if layer.parent.export:
+			return
+		xMin, yMin = frame.origin.x, frame.origin.y
+		xMax = xMin + frame.size.width
+		yMax = yMin + frame.size.height
+		self.drawCross(xMin, xMax, yMin, yMax)
 	
 	@objc.python_method
-	def drawCrossOverLayer( self, Layer, lineWidth ):
+	def drawCrossOverLayer(self, Layer, lineWidth):
 		try:
 			# determine italic angle:
 			try:
 				thisFont = Layer.parent.parent
 				thisMaster = thisFont.masters[Layer.associatedMasterId]
-				italicAngle = radians( thisMaster.italicAngle ) #% 90.0 )
+				italicAngle = radians(thisMaster.italicAngle) #% 90.0)
 			except:
 				italicAngle = 0.0
-			tangens = tan( italicAngle )
 			
 			# determine coordinates:
 			Master = Layer.associatedFontMaster()
@@ -47,29 +69,20 @@ class ShowExportStatus(ReporterPlugin):
 			xMax = Layer.width
 			yMin = Master.descender
 			xMin = 0
-			italicBottomOffset = tangens * (yMin - halfXHeight)
-			italicTopOffset = tangens * (yMax - halfXHeight)
 			
 			# draw cross:
-			NSColor.redColor().set()
-			cross = NSBezierPath.bezierPath()
-			cross.moveToPoint_( NSPoint(xMin + italicBottomOffset, yMin) )
-			cross.lineToPoint_( NSPoint(xMax + italicTopOffset,    yMax) )
-			cross.moveToPoint_( NSPoint(xMax + italicBottomOffset, yMin) )
-			cross.lineToPoint_( NSPoint(xMin + italicTopOffset,    yMax) )
-			cross.setLineWidth_( lineWidth )
-			cross.stroke()
+			self.drawCross(xMin, xMax, yMin, yMax, lineWidth=lineWidth, italicAngle=italicAngle, halfXHeight=halfXHeight)
 		except Exception as e:
-			self.logToConsole( "drawCrossOverLayer_: %s" % str(e) )
+			self.logToConsole("drawCrossOverLayer_: %s" % str(e))
 
 	@objc.python_method
-	def background( self, Layer ):
+	def background(self, Layer):
 		try:
 			thisGlyph = Layer.glyph()
 			if thisGlyph and not thisGlyph.export:
 				self.drawCrossOverLayer(Layer, 1.0 / self.getScale())
 		except Exception as e:
-			self.logToConsole( "background: %s" % str(e) )
+			self.logToConsole("background: %s" % str(e))
 
 	@objc.python_method	
 	def inactiveLayerBackground(self, Layer):
@@ -78,7 +91,7 @@ class ShowExportStatus(ReporterPlugin):
 			if thisGlyph and not thisGlyph.export:
 				self.drawCrossOverLayer(Layer, 1.0 / self.getScale())
 		except Exception as e:
-			self.logToConsole( "inactiveLayer: %s" % str(e) )
+			self.logToConsole("inactiveLayer: %s" % str(e))
 			
 	@objc.python_method	
 	def inactiveLayer(self, Layer):
@@ -92,9 +105,9 @@ class ShowExportStatus(ReporterPlugin):
 			if thisGlyph and not thisGlyph.export:
 				self.drawCrossOverLayer(Layer, 1.0 / self.getScale())
 		except Exception as e:
-			self.logToConsole( "preview: %s" % str(e) )
+			self.logToConsole("preview: %s" % str(e))
 
-	def needsExtraMainOutlineDrawingForInactiveLayer_( self, Layer ):
+	def needsExtraMainOutlineDrawingForInactiveLayer_(self, Layer):
 		return True
 
 	@objc.python_method
